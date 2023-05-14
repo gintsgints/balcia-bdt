41258804b03e817e31ca95e19709540d5109935b
Gints Polis <polis.gints@gmail.com>
2023-05-14 22:00:50 +0300

    Do not add comma to last column

--- src/sqlite_adapter.rs
+++ src/sqlite_adapter.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::io::stdout;
 use serde::Serialize;
-use handlebars::{Context, Handlebars, Helper, Output, RenderContext, RenderError};
+use handlebars::{Context, Handlebars, Helper, Output, RenderContext, RenderError, handlebars_helper};
 
 use crate::Bdt;
 
@@ -10,6 +10,41 @@ pub struct SqliteAdapter {}
 #[derive(Debug, Serialize)]
 struct BdtList {
     tables: Vec<Bdt>,
+}
+
+// handlebars_helper!(last: |array: , index: usize| if array.len() == index { ",".to_string() } else { "".to_string()});
+
+pub fn array_last_comma(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> Result<(), RenderError> {
+    let length = h
+        .param(0)
+        .as_ref()
+        .and_then(|v| v.value().as_array())
+        .map(|arr| arr.len())
+        .ok_or(RenderError::new(
+            "Param 0 with 'array' type is required for array_last_comma helper",
+        ))?;
+
+    let index = h
+        .param(1)
+        .as_ref()
+        .and_then(|v| v.value().as_u64())
+        .ok_or(RenderError::new(
+            "Param 1 with 'usize' type is required for array_last_comma helper",
+        ))?;
+
+    if length as u64 != index + 1 {
+        write!(out, ",")?;
+    } else {
+        write!(out, "")?;
+    }
+
+    Ok(())
 }
 
 fn some_helper(
@@ -36,6 +71,7 @@ impl SqliteAdapter {
     pub fn write_bdt(tables: Vec<Bdt>) -> Result<(), Box<dyn Error>> {
         let mut handlebars = Handlebars::new();
         handlebars.register_helper("some", Box::new(some_helper));
+        handlebars.register_helper("last", Box::new(array_last_comma));
         handlebars
             .register_template_file("template", "./render/sqlite.hbs")
             .unwrap();
